7b6a75f52a7b7a337eb82d42a5a3a515b079d982
janiv <jani.vilkki@gmail.com>
2023-12-02 16:32:39 -0600

    Day 2 done

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -8,14 +8,10 @@ GREEN_PATTERN = r"([\d,]+)[ ]+green"
 BLUE_PATTERN = r"([\d,]+)[ ]+blue"
 
 
-def day_2_solver(filename: str, red: int, green: int, blue: int) -> int:
+def day_2_solver_part1(filename: str, red: int, green: int, blue: int) -> int:
     total = 0
-    try:
-        file = open(filename, 'r')
-    except IOError:
-        print("Dat file no exist")
+    file = open(filename, 'r')
     lines = file.readlines()
-
     for line in lines:
         data = line.split(':')
         pulls = data[1].split(';')
@@ -24,9 +20,26 @@ def day_2_solver(filename: str, red: int, green: int, blue: int) -> int:
             r = re.search(RED_PATTERN, pull)
             g = re.search(GREEN_PATTERN, pull)
             b = re.search(BLUE_PATTERN, pull)
-            print(r)
+            if r:
+                r = r.group().split()[0]
+                r = int(r)
+                if(r>red):
+                    addable= False
+                    break
         
+            if g:
+                g = g.group().split()[0]
+                g = int(g)
+                if(g>green):
+                    addable= False
+                    break
 
+            if b:
+                b = b.group().split()[0]
+                b = int(b)
+                if(b > blue):
+                    addable= False
+                    break
         if addable:
             gameInfo = data[0].split()
             gameNum = gameInfo[1]
@@ -35,4 +48,40 @@ def day_2_solver(filename: str, red: int, green: int, blue: int) -> int:
 
     return total
 
-day_2_solver(TEST_FILE_NAME, 12, 13, 14)
+
+def day_2_part2(filename: str) -> int:
+    total = 0
+    file = open(filename, 'r')
+    lines = file.readlines()
+    for line in lines:
+        data = line.split(':')
+        pulls = data[1].split(';')
+        min_r = 0
+        min_g = 0
+        min_b = 0
+        for pull in pulls:
+            r = re.search(RED_PATTERN, pull)
+            g = re.search(GREEN_PATTERN, pull)
+            b = re.search(BLUE_PATTERN, pull)
+            if r:
+                r = r.group().split()[0]
+                r = int(r)
+                min_r = max(r, min_r)
+        
+            if g:
+                g = g.group().split()[0]
+                g = int(g)
+                min_g = max(g, min_g)
+
+            if b:
+                b = b.group().split()[0]
+                b = int(b)
+                min_b = max(b, min_b)
+        power = min_r * min_g * min_b
+        total += power
+
+    return total
+
+
+print(day_2_part2(INPUT_FILE_NAME))
+
