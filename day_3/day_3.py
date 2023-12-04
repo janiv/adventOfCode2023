3a863e0a1a2bddaf330e4087223bf4c0c5471488
janiv <jani.vilkki@gmail.com>
2023-12-04 13:26:17 -0600

    Day 3 done, my stupid solutions keep working!

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -40,10 +40,10 @@ def isValidPart(lines: List[str], above: int, below: int, left: int, right: int)
         
     return False
 
-def substitute(start: int, end: int)->str:
+def substitute(start: int, end: int, character: str)->str:
     rep_str = ""
     for i in range(start, end):
-        rep_str = rep_str + "a"
+        rep_str = rep_str + character
     return rep_str
 
 def day_3_part1(filename: str)-> int:
@@ -67,21 +67,42 @@ def day_3_part1(filename: str)-> int:
                 total += int(num)
             #We need to do this to handle duplicates of same number
             #Is it dumb? yes. Does it work? also yes.
-            filler = substitute(left, right)
+            filler = substitute(left, right, "a")
             line = line.replace(num, filler, 1)
             
     print(total)
     return total
 
-def num_search_helper(line: str, number: int, left, right):
-    while re.search(number, line):
-        num = re.search(number, line)
+def num_search_helper(line: str, left: int, right: int) -> List[int]:
+    numbers = []
+    while re.search(NUM_PAT, line):
+        num = re.search(NUM_PAT, line)
         num_span = num.span()
         for i in range(num_span[0], num_span[1]):
-            if left <= i <= right:
-                return True
+            if left-1 <= i <= right:
+                numbers.append(num.group())
+                break
+        filler = substitute(num_span[0], num_span[1], 'n')
+        line = line.replace(num.group(), filler, 1)
+    return numbers
             
+def find_nums(lines: List[str], line_number: int, left: int, right: int) -> List[int]:
+    #First find all nums near rectangle
+    #above
+    nums = []
+    if line_number > 0:
+        above = line_number -1
+        nums_above = num_search_helper(lines[above], left, right)
+        nums += nums_above
+    if line_number < len(lines) -1:
+        below = line_number + 1
+        nums_below = num_search_helper(lines[below], left, right)
+        nums += nums_below
+    nums_adjacent = num_search_helper(lines[line_number], left, right)
+    nums += nums_adjacent
 
+    return nums
+    
 
 def day_3_part2(filename: str) -> int:
     total = 0
@@ -95,19 +116,18 @@ def day_3_part2(filename: str) -> int:
         gears = re.findall(GEAR_PAT, line)
         for gear in gears:
             parts = []
-            numbers = re.findall(NUM_PAT, line)
             left = re.search("\\" + gear, line).start()
             right = re.search("\\" + gear, line).end()
-            for num in numbers:
-                n_left = re.search(num, line).start()
-                n_right = re.search(num, line).end()
-                if (num_search_helper(line, num, n_left, n_right)):
-                    parts.append(num)
-                filler = substitute(n_left, n_right)
-                line = line.replace(num, filler, 1)
-            print("Gear on line: ", i, " left: ", left, " right:", right)
-    return 0
+            parts = find_nums(lines, i, left, right)
+            if len(parts) == 2:
+                ratio = int(parts[0]) * int(parts[1])
+                total += ratio
+            filler = substitute(left, right, "g")
+            line = line.replace(gear, filler, 1)
+            
+    return total
+
 
 
 s = "String"
-day_3_part2("day_3_test.txt")
+print(day_3_part2("day_3_input.txt"))
